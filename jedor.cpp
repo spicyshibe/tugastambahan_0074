144e3b3afab55bbf8fe6d0c0796a0cd6758a1231
spicyshibe <rafierasyw@gmail.com>
2025-05-01 16:52:36 +0700

    mengisi void input data

--- jedor.cpp
+++ jedor.cpp
@@ -13,4 +13,28 @@ class segitiga{
 
 
     void inputdata(){
-   
+        cout << "masukkan alas segitiga : " << endl;
+        cin >> a;
+        cout << "masukkan tinggi segitiga : " << endl;
+        cin >> t;
+
+        if (a % 2 == 1)
+        {
+            cekganjil = false;
+
+        }
+        
+        else
+        {
+            cekganjil = true;
+
+        }
+        
+        
+
+    }
+
+
+
+    
+
